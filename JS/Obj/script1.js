41420406947e45da6dd50d1c9f715b9fbf84aef3
CodeWithAbdur <106903848+codewithabdur@users.noreply.github.com>
2023-06-02 17:34:35 +0530

    Obj

---
+++ JS/Obj/script1.js
@@ -0,0 +1,9 @@
+var c = "script1" ;
+console.log(c) ;
+
+(function print1(){
+    var b = "Part1"
+    console.log(print1)
+})();
+
+// print1()
---
+++ JS/Obj/script2.js
@@ -0,0 +1,9 @@
+var c = "script2" ;
+console.log(c) ;
+
+(function print2(){
+    var b = "Part2"
+    console.log(print2)
+})();
+
+// print2()
